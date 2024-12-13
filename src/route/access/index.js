715626b20454ce2c8f8310ade16ca3068df44c2c
Thanh968 <thanhpitbull@gmail.com>
2024-12-13 17:11:15 +0700

    add check auth

--- src/route/access/index.js
+++ src/route/access/index.js
@@ -2,9 +2,17 @@
 const express = require(`express`);
 const route = express.Router();
 const AccessController = require(`../../controllers/access.controller`);
-const {asyncErrorHandling} = require(`../../helper`)
+const {asyncErrorHandling} = require(`../../helper`);
+const { checkAuthentication } = require("../../auth/auth");
 
 route.post(`/shop/signup`,asyncErrorHandling(AccessController.signUp));
 route.post(`/shop/login`, asyncErrorHandling(AccessController.login));
+
+route.use(asyncErrorHandling(checkAuthentication));
+route.get(`/shop/example`, (req, res, next) => {
+    return res.status(200).json({
+        message: "Hello client",
+    });
+});
 
 module.exports = route;
